318906b0ea9cfb63f3b29a99ec8fae18e6fea9e8
Bjornton <bj.severson34@gmail.com>
2022-09-19 03:13:29 -0500

    Added a random function that will make the characters be random in the password. And added the character limit

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,38 @@ function writePassword() {
     passwordText.value = password;
   }
 }
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min * (1 - rand) + (rand * max))
+}
+ 
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+ 
+function generatePassword() {
+ 
+  while (true) {
+    var userInput = window.prompt("Enter how long you would like the password to be. (The password must be at least 8 characters and at most 128 characters).")
+ 
+    if (userInput === null) {
+      return
+    }
+ 
+    var passwordLength = parseInt(userInput);
+ 
+    if (isNaN(passwordLength)) {
+      window.alert("Please enter a number.")
+    } else if( passwordLength < 8 || passwordLength > 128) {
+      window.alert("Invalid password length. Please enter a number between 8 and 128.")
+    } else {
+      break
+    }
+  }
 
 // Add event listener to generate button
 generateBTN.addEventListener("click", writePassword);
